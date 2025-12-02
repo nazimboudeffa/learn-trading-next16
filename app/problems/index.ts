45a2e591daab92eb8323e8225416cf1f1693697d
Nazim Boudeffa <boudeffa@gmail.com>
2025-12-02 02:42:27 +0100

    little fix

--- app/problems/index.ts
+++ app/problems/index.ts
@@ -1,11 +1,9 @@
-import { chart1 } from "./example-chart";
 import { chart2008 } from "./crash-financier-2008";
 import { chartCovid } from "./krach-covid-2020";
 import { chartBTC2017 } from "./bullrun-bitcoin-2017";
 import { chartFTX2022 } from "./crash-ftx-2022";
 
 export const problems = [
-    chart1,
     chart2008,
     chartCovid,
     chartBTC2017,
